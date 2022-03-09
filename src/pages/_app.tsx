c650b9e9a5ab14695178fab2de72c8f272a5d749
Reo Ito <leo.0210.ito@gmail.com>
2022-03-10 00:52:23 +0900

    Tailwindcss(ui)でheaderを作ってみる

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout'
+import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
       <Component {...pageProps} />
     </Layout>
   )
